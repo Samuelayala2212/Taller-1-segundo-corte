436e1e97af5198aacd4b93ec531a4bc36225732d
Samuelayala2212 <samuel.ayala02@usa.edu.co>
2025-03-27 23:32:36 -0500

    Add files via upload

--- Sin título0.py
+++ Sin título0.py
@@ -7,15 +7,40 @@ while True:
    print("4 = Salir")
    entrada=input("Elija una opción: ")
    if entrada == "1":
-       print("Vertices")
-       vertices=int(input("Ingrese la cantidad de vertices: "))
+       print("Vertices")          
+       while True:
+           vertices=int(input("Ingrese minimo 3 vertices: "))
+           if vertices<3:
+               print("Debe ingresar minimo 3 vertices")
+           else:
+               break
        for i in range(vertices):
            x=input(f"Vertice {i+1} -x ")
            y=input(f"Vertice {i+1} -y ")
            z=input(f"Vertice {i+1} -z ")
            v.append(f"v {x} {y} {z}")
    elif entrada=="2":
-       print("Caras")
-       print(v)
+       if not v:
+           print("Primero se necesitan los vertices")
+           continue
+       print("\nCaras")
+       for i in range(len(v) - 2):
+           c.append(f"f {' '.join(str(i+1) for i in range(len(v)))}")
+   elif entrada=="3":
+       for vertice in v:
+            print(vertice)
+       for cara in c:
+            print(cara)
+   elif entrada == "4":
+       break
+
+
+        
+     
+  
+   
+      
+           
+       
            
        
